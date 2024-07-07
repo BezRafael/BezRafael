3160e9ca8292f81597f62ac02c0272516e3aaafd
BezRafael <rafaelb120934@gmail.com>
2024-07-07 17:41:36 -0300

    adicionando (popup)

---
+++ src/public/js/index.js
@@ -0,0 +1,33 @@
+
+//Função para abrir e fechar a caixa dialogo 1
+function abrir_dialog1(){
+    let dialog_1 = document.getElementById('dialog_1')
+    dialog_1.show()
+}
+
+function fechar_dialog1(){
+    let dialog_1 = document.getElementById('dialog_1')
+    dialog_1.close()
+}
+
+//Função para abrir e fechar a caixa dialogo 2
+function abrir_dialog2(){
+    let dialog_2 = document.getElementById('dialog_2')
+    dialog_2.show()
+}
+
+function fechar_dialog2(){
+    let dialog_2 = document.getElementById('dialog_2')
+    dialog_2.close()
+}
+
+//Função para abrir e fechar a caixa dialogo 3
+function abrir_dialog3(){
+    let dialog_3 = document.getElementById('dialog_3')
+    dialog_3.show()
+}
+
+function fechar_dialog3(){
+    let dialog_3 = document.getElementById('dialog_3')
+    dialog_3.close()
+}
